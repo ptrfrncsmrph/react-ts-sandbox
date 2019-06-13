b3d7d73e2c23277c930e1e9c3bac7739da99cede
Peter Murphy <peter.francis.murphy@gmail.com>
2019-06-13 17:09:23 -0400

    Experiment with grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React, { FunctionComponent } from "react";
 import "./App.scss";
 
+const headings = Array(20)
+  .fill(null)
+  .map((_, i) => `Hello ${i}`);
+
 const App: FunctionComponent = () => {
   return (
     <main className="App">
       <h1>Hello</h1>
+      <h2>Hello smaller</h2>
+      {headings.map((heading, i) => (
+        <h3 key={i}>{heading}</h3>
+      ))}
     </main>
   );
 };
